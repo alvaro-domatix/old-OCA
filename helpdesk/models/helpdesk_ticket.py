817364468766cd47921d71931ef77abadaf9582d
miguelestevan <miguel@domatix.com>
2019-06-17 11:33:15 +0200

    add new filters, group by. Create on table category, channel and tags, new view for team form, add field to stages form

--- helpdesk/models/helpdesk_ticket.py
+++ helpdesk/models/helpdesk_ticket.py
@@ -19,6 +19,53 @@ class HelpdeskTicket(models.Model):
         'res.users',
         string='Assigned user',)
 
+    active = fields.Boolean(
+        default=True)
+
+    kanban_state = fields.Selection(
+        [('normal', 'Grey'),
+         ('done', 'Green'),
+         ('blocked', 'Red')],
+        string='kanban state',
+        copy=False,
+        default="normal",
+        required=True,
+        help="A task's kanban state indicates special situations affecting it: \n"
+              " * Grey us the default situation\n"
+              " * Red indicates something is preventing the progress of this task\n"
+              " * Green indicates the task is ready to be pulled to the next stage")
+
+    kanban_state_label = fields.Char(
+        compute='_compute_kanban_state_label',
+        string='Kanban state',
+        track_visibility="onchange")
+
+    legend_priority = fields.Char(
+        string='Starred Explanation',
+        translate=True,
+        help="Explanation text to help users using the star on tasks or issues in this stage.")
+
+    legend_blocked = fields.Char(
+        string='Red kanban Label',
+        default=lambda s: _('Blocked'),
+        translate=True,
+        required=True,
+        help='Override the default value displayed for the blocked state for kanban selection, when the task or issue is in that stage.')
+
+    legend_done = fields.Char(
+        string='Green kanban Label',
+        default=lambda s: _('Ready for Next Stage'),
+        translate=True,
+        required=True,
+        help='Override the default value displayed for the done state for kanban selection, when the task or issue is in that stage.')
+
+    legend_normal = fields.Char(
+        string='Grey Kanban Label',
+        default=lambda s: _('In progress'),
+        translate=True,
+        required=True,
+        help='Override the default value displayed for the normal state for kanban selection, when the task or issue is in that stage.')
+
     @api.model
     def _read_group_stage_ids(self, stages, domain, order):
         stage_ids = self.env['helpdesk.ticket.stage'].search([])
@@ -43,6 +90,7 @@ class HelpdeskTicket(models.Model):
     closed_date = fields.Datetime(string='Closed Date')
     closed = fields.Boolean(related='stage_id.closed')
     unattended = fields.Boolean(related='stage_id.unattended')
+
     tag_ids = fields.Many2many('helpdesk.ticket.tag')
     company_id = fields.Many2one(
         'res.company',
@@ -56,8 +104,10 @@ class HelpdeskTicket(models.Model):
         help='Channel indicates where the source of a ticket'
              'comes from (it could be a phone call, an email...)',
     )
+
     category_id = fields.Many2one('helpdesk.ticket.category',
                                   string='Category')
+
     team_id = fields.Many2one('helpdesk.ticket.team')
     priority = fields.Selection(selection=[
         ('0', _('Low')),
@@ -65,6 +115,7 @@ class HelpdeskTicket(models.Model):
         ('2', _('High')),
         ('3', _('Very High')),
     ], string='Priority', default='1')
+
     attachment_ids = fields.One2many(
         'ir.attachment', 'res_id',
         domain=[('res_model', '=', 'website.support.ticket')],
@@ -83,6 +134,15 @@ class HelpdeskTicket(models.Model):
             self.partner_name = self.partner_id.name
             self.partner_email = self.partner_id.email
 
+    @api.depends('stage_id', 'kanban_state')
+    def _compute_kanban_state_label(self):
+        for ticket in self:
+            if ticket.kanban_state == 'normal':
+                ticket.kanban_state_label = ticket.legend_normal
+            elif ticket.kanban_state == 'blocked':
+                ticket.kanban_state_label = ticket.legend_blocked
+            else:
+                ticket.kanban_state_label = ticket.legend_done
     # ---------------------------------------------------
     # CRUD
     # ---------------------------------------------------
@@ -112,6 +172,8 @@ class HelpdeskTicket(models.Model):
                     vals['closed_date'] = now
             if vals.get('user_id'):
                 vals['assigned_date'] = now
+            if 'kanban_state' not in vals:
+                vals['kanban_state'] = 'normal'
 
         res = super(HelpdeskTicket, self).write(vals)
 
