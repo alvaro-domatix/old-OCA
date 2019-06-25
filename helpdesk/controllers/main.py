ef2cb1e936cbd2757b33d94b177a8091e45e755e
samuel <samuel@domatix.com>
2019-06-25 13:44:52 +0200

    Creación de tickets desde portal

--- helpdesk/controllers/main.py
+++ helpdesk/controllers/main.py
@@ -1,6 +1,8 @@
 import werkzeug
 import logging
 import odoo.http as http
+import requests
+from openerp.http import request
 _logger = logging.getLogger(__name__)
 
 
@@ -14,6 +16,36 @@ class HelpdeskTicketController(http.Controller):
             values[field_name] = field_value
         ticket = http.request.env['helpdesk.ticket'].sudo().\
             search([('id', '=', values['ticket_id'])])
-        ticket.stage_id = values.get('stage_id')
+        ticket.stage_id = int(values.get('stage_id'))
 
         return werkzeug.utils.redirect("/my/ticket/" + str(ticket.id))
+
+    @http.route('/new/ticket', type="http", auth="user", website=True)
+    def create_new_ticket(self, **kw):
+
+        categories = http.request.env['helpdesk.ticket.category'].sudo().search([('active', '=', True)])
+        email = http.request.env.user.email
+        name = http.request.env.user.name
+
+        return http.request.render('helpdesk.portal_create_ticket', {'categories': categories, 'email': email, 'name': name})
+
+    @http.route('/submitted/ticket', type="http", auth="user", website=True, csrf=True)
+    def submit_ticket(self, **kw):
+        vals = {}
+
+        for field_name, field_value in kw.items():
+            vals[field_name] = field_value
+
+        new_ticket = {
+                    'partner_name': vals['name'],
+                    'category_id': vals['category'],
+                    'partner_email': vals['email'],
+                    'description': vals['description'],
+                    'name': vals['subject'],
+                    # 'attachment_ids': vals['attachment'],
+                    'channel_id':request.env['helpdesk.ticket.channel'].sudo().search([('name', '=', 'Web')]).id,
+                    'partner_id': request.env['res.partner'].sudo().search([('name', '=', vals['name']), ('email', '=', vals['email'])]).id
+                 }
+        new_ticket_id = request.env['helpdesk.ticket'].sudo().create(new_ticket)
+
+        return werkzeug.utils.redirect("/my/tickets")
