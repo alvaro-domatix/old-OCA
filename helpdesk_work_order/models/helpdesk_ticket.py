30010f1df9b2b59db831b174b617534661e5349e
alvaro <alvaro@domatix.com>
2019-06-27 10:52:04 +0200

    [FIX] Delted smartbutton, hide button to create WO instead of show warning

--- helpdesk_work_order/models/helpdesk_ticket.py
+++ helpdesk_work_order/models/helpdesk_ticket.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models, tools, _
+from odoo import fields, models, _
 from odoo.exceptions import UserError
 
 
@@ -14,39 +14,23 @@ class HelpdeskTicket(models.Model):
         if not self.partner_id:
             raise UserError(_(
                 "The Ticket must have partner to create a work order."))
-
         work_order_obj = self.env['work.order']
         header = 'Helpdesk Ticket: ' + self.number + ' - ' + self.name + '\n'
-
-        if self.number in self.env['work.order'].search([]).mapped('origin_document'):
-            raise UserError(_(
-                "This ticket already has a work order."))
 
         work_order = work_order_obj.create({
             'partner_id': self.partner_id.id,
             'origin_document': self.number,
-            'address_id': self.partner_id.id,
-            'priority': self.priority,
             'ticket_description':
                 (header + self.description) if self.description else header,
         })
+        work_order.onchange_partner_id()
 
-        message = _("This Work Order has been created from: <a href=# data-oe-model=helpdesk.ticket data-oe-id=%d>%s</a>") % (self.id, self.number)
+        message = _("This Work Order has been created from: \
+            <a href=# data-oe-model=helpdesk.ticket data-oe-id=%d>%s</a>") % (
+            self.id, self.number)
+
         work_order.message_post(body=message)
 
         self.write({
             'work_order_id': work_order.id
         })
-
-    def action_view_work_order_id(self):
-        self.ensure_one()
-        return {
-            'name': _('Work orders'),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'work.order',
-            'type': 'ir.actions.act_window',
-            'view_id': self.env.ref('work_order.view_work_order_form').id,
-            'target': 'current',
-            'res_id': self.work_order_id.id,
-        }
